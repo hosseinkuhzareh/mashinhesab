60c93f81ec3a5457f9173f01fda4a2f4456a9b62
hosseinkuhzareh <75494182+hosseinkuhzareh@users.noreply.github.com>
2020-12-04 21:01:52 +0330

    ماشین حساب هستش

---
+++ mashinhesab.py
@@ -0,0 +1,21 @@
+print("         hello world")
+print("--------------------------------")
+print("     my name:hosseinkuhzare")
+print("     my telegram id:@Hsnsjs")
+print("--------------------------------")
+print("            Start")
+num_yek=int(input("enter numder1:"))
+num_do=int(input("enter numder2:"))
+print("jamh:+",num_yek+num_do)
+print("tafrig -:",num_yek-num_do)
+print("zarb × :",num_yek*num_do)
+print("tagsim ÷ :",num_yek/num_do)
+
+print("       finish")
+
+
+
+
+
+
+
